42d071a2507d5812d575c0875db1a0008bba86e9
Oliver Kaufmann <okaufmann91@gmail.com>
2021-04-24 15:28:33 +0200

    wip

--- resources/views/livewire/movie-finder.blade.php
+++ resources/views/livewire/movie-finder.blade.php
@@ -4,11 +4,14 @@
     </div>
     <div>
         @forelse($movies as $movie)
-            <div class="odd:bg-gray-200 even:bg-gray-100">
+            <div class="odd:bg-gray-200 even:bg-gray-100" wire:loading.remove>
                 <livewire:movie-item :movie="$movie" :key="$movie->getId()"/>
             </div>
         @empty
-            <div>No movies found...</div>
+            <div wire:loading.remove>No movies found...</div>
         @endforelse
+            <div wire:loading>
+                Loading results...
+            </div>
     </div>
 </div>
