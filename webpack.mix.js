947efd8780697e6c7510fffb6f34d72350fba0ee
Oliver Kaufmann <okaufmann91@gmail.com>
2021-05-26 22:15:09 +0200

    enable cache busting for assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,3 +16,8 @@ mix.js("resources/js/app.js", "public/js")
     .postCss("resources/css/app.css", "public/css", [
         require("tailwindcss"),
     ]);
+
+if (mix.inProduction()) {
+    mix.version();
+}
+
