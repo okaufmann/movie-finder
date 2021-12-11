8c205cf5b7c498fda27f8f743b889df87f888832
Oliver Kaufmann <okaufmann91@gmail.com>
2021-12-11 18:09:34 +0100

    upgrade cs fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,6 +4,9 @@ use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
 
 $rules = [
+    '@PSR12' => true,
+    '@PSR12:risky' => true,
+    'psr_autoloading' => true,
     'array_syntax' => [
         'syntax' => 'short',
     ],
@@ -15,7 +18,6 @@ $rules = [
         ],
     ],
     'blank_line_after_namespace' => true,
-    'blank_line_after_opening_tag' => true,
     'blank_line_before_statement' => [
         'statements' => [
             0 => 'return',
@@ -25,14 +27,15 @@ $rules = [
     'cast_spaces' => true,
     'class_attributes_separation' => [
         'elements' => [
-            0 => 'method',
+            'method' => 'one',
+            'trait_import' => 'none',
+            'property' => 'one'
         ],
     ],
     'class_definition' => true,
     'concat_space' => [
         'spacing' => 'none',
     ],
-    'declare_equal_normalize' => true,
     'elseif' => true,
     'encoding' => true,
     'full_opening_tag' => true,
@@ -46,10 +49,8 @@ $rules = [
     'indentation_type' => true,
     'linebreak_after_opening_tag' => true,
     'line_ending' => true,
-    'lowercase_cast' => true,
-    'lowercase_constants' => true,
+    'constant_case' => true,
     'lowercase_keywords' => true,
-    'lowercase_static_reference' => true,
     'magic_method_casing' => true,
     'magic_constant_casing' => true,
     'method_argument_space' => true,
@@ -59,15 +60,12 @@ $rules = [
             0 => 'extra',
             1 => 'throw',
             2 => 'use',
-            3 => 'use_trait',
         ],
     ],
-    'no_blank_lines_after_class_opening' => true,
     'no_blank_lines_after_phpdoc' => true,
     'no_closing_tag' => true,
     'no_empty_phpdoc' => true,
     'no_empty_statement' => true,
-    'no_leading_import_slash' => true,
     'no_leading_namespace_whitespace' => true,
     'no_mixed_echo_print' => [
         'use' => 'echo',
@@ -87,15 +85,15 @@ $rules = [
     'no_trailing_whitespace_in_comment' => true,
     'no_unneeded_control_parentheses' => true,
     'no_whitespace_before_comma_in_array' => true,
-    'no_whitespace_in_blank_line' => true,
     'normalize_index_brace' => true,
     'not_operator_with_successor_space' => true,
     'object_operator_without_whitespace' => true,
     'ordered_imports' => [
-        'sortAlgorithm' => 'alpha',
+        'sort_algorithm' => 'alpha',
+        'imports_order' => ['class', 'function', 'const'],
     ],
     'phpdoc_indent' => true,
-    'phpdoc_inline_tag' => true,
+    'phpdoc_inline_tag_normalizer' => true,
     'phpdoc_no_access' => true,
     'phpdoc_no_package' => true,
     'phpdoc_no_useless_inheritdoc' => true,
@@ -106,9 +104,7 @@ $rules = [
     'phpdoc_trim' => true,
     'phpdoc_types' => true,
     'phpdoc_var_without_name' => true,
-    'short_scalar_cast' => true,
     'single_blank_line_at_eof' => true,
-    'single_blank_line_before_namespace' => true,
     'single_class_element_per_statement' => true,
     'single_import_per_statement' => true,
     'single_line_after_imports' => true,
@@ -122,24 +118,16 @@ $rules = [
     'standardize_not_equals' => true,
     'switch_case_semicolon_to_colon' => true,
     'switch_case_space' => true,
-    'ternary_operator_spaces' => true,
-    'trailing_comma_in_multiline_array' => true,
+    'trailing_comma_in_multiline' => ['elements' => ['arrays', 'parameters']],
     'trim_array_spaces' => true,
     'unary_operator_spaces' => true,
-    'visibility_required' => [
-        'elements' => [
-            0 => 'method',
-            1 => 'property',
-        ],
-    ],
     'whitespace_after_comma_in_array' => true,
     'no_unused_imports' => true,
-    'psr4' => true,
     'method_chaining_indentation' => true,
     'explicit_string_variable' => true,
 ];
 
-$finder = Finder::create()
+$finder = Symfony\Component\Finder\Finder::create()
     ->in([
         __DIR__ . '/app',
         __DIR__ . '/config',
@@ -153,7 +141,7 @@ $finder = Finder::create()
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
 
-return Config::create()
+return (new Config)
     ->setFinder($finder)
     ->setRules($rules)
     ->setRiskyAllowed(true)
